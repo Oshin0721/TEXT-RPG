8a203675b5919325f09a8443c32a7e80c56c8157
Oshin0721 <ehdgk3210@naver.com>
2025-06-19 21:02:40 +0900

    iteam update

---
+++ TEXT-RPG/HealthPotion.h
@@ -0,0 +1,18 @@
+#ifndef HEALTHPOTTION_H_
+#define HEALTHPOTTION_H_
+
+#include "Item.h"
+
+class HealthPotion : public Item
+{
+private:
+	std::string name;       //아이템 이름
+	int healthRestore;      //회복할 수치
+
+public:
+	HealthPotion();         //생성자
+	std::string getName() const override;  //이름 반환
+	void use(Character* character) override;
+};
+
+#endif
